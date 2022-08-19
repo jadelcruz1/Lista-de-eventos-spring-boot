2676f2b2dca9f23bcaef76951d38b148682af43e
jadelcruz1 <jardelcruz6@gmail.com>
2022-08-19 16:50:49 -0300

    Requisições especificas com a anotacao @PathValiable

--- eventoapp/src/main/java/com/eventoapp/controllers/EventoController.java
+++ eventoapp/src/main/java/com/eventoapp/controllers/EventoController.java
@@ -3,6 +3,7 @@ package com.eventoapp.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -35,5 +36,15 @@ public class EventoController {
 		mv.addObject("eventos", eventos);
 		return mv;
 	}
+	
+	@RequestMapping("/{codigo}")
+	public ModelAndView detalhesEvento(@PathVariable ("codigo")long codigo) {
+		Evento evento = er.findByCodigo(codigo);
+		ModelAndView mv = new ModelAndView("evento/detalhesEvento");
+		mv.addObject("evento", evento);
+		return mv;
+		
+	}
+ }
 
-}
+
